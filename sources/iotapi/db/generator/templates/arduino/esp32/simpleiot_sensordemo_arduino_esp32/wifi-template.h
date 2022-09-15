629af8e51bc9aa48173bc0e9463c0d4e2413385e
Ramin Firoozye <framin@amazon.com>
2022-09-15 16:14:20 -0400

    Fixes for sensordemo code generation issues

--- sources/iotapi/db/generator/templates/arduino/esp32/simpleiot_sensordemo_arduino_esp32/wifi-template.h
+++
@@ -1,8 +0,0 @@
-/*
- * © 2021 Amazon Web Services, Inc. or its affiliates. All Rights Reserved.
- * 
- * Update these with your own WiFi credentials
- */
-
-const char WIFI_SSID[]="*** your wifi SSID ***";
-const char WIFI_PASSWORD[]="*** your wifi Password ***";
